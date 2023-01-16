9037ebfcb872abaf59420953deb324f7cb3d2bb4
AiutoPcAmico <andrea@andreafelappi.it>
2023-01-16 13:02:42 +0100

    Working backend. TODO: Create frontend

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ import { initDevice } from "./iot/index.js";
 
 import express from "express";
 
-const app = express();
 
 // downloading remote cert to connect
 await checkMasterCertificate();
 
 // connect to mqtt queue
 await initDevice();
-
-app.listen(9000, () => {
-    console.log('Server listening on port 9000');
-});
